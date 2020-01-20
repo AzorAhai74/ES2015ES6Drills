9fbedac403de3123f47d7a4dbf6a3a7289e6bcdf
byron baldwin <byronsbaldwin@gmail.com>
2020-01-19 22:00:17 -0600

    No. 10

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ function favMovie(name = 'World', movie = 'The Room') {
     console.log(`My name is ${name} and my favorite movie is ${movie}`);
 }
 
-favMovie('Byron');
+favMovie('Byron', 'Gladiator');
 
